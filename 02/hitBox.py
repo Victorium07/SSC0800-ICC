1fa8b6042dbeb69e2e5dc3d065524a7af12f3ddd
Victor <victor.h.alves7@gmail.com>
2023-08-29 15:12:04 -0300

    renomeando variáveis das funções para facilitar a compreensão. Novas funções para tornar as entradas em inteiros e separação de eixos.

--- 02/hitBox.py
+++ 02/hitBox.py
@@ -12,34 +12,50 @@
 
 #funções úteis
 
-#opção alternativa: comparar x separadamente e depois y separadamente.
-def interseptCheck(e1=[0,0,0,0], e2 = [0,0,0,0]):
-    if(((e2[0] <= e1[2]) and (e2[1] <= e1[3])) or ((e2[2] >= e1[0]) and (e2[3] >= e1[1]))):
+#converte a lista de str em int
+def conversorEntradas(listaCoordenadas):
+    obs = [int(numero) for numero in listaCoordenadas]
+    return obs
+
+#separa as entradas em suas correspondêntes dimensões
+def eixo(entrada):
+    return ([entrada[0],entrada[2]], [entrada[1], entrada[3]])
+
+#avalia se há sobreposição
+def interseptCheck(x1, x2, y1, y2):
+    if(((x2[0] <= x1[1]) and (y2[0] <= y1[1])) or ((x2[0] >= x1[1]) and (y2[1] >= y1[0]))):
         return True
     else: 
         return False
 
-def linearOverlap(e1, e2):
-    if (e1[0] >= e2[0]):
-        valorInicial = e1[0]
-    else: valorInicial = e2[0]
-    if (e1[1] >= e2[1]):
-        valorFinal = e2[1]
-    else: valorFinal = e1[1]
+#devolve qual é a sobreposição linear
+def linearOverlap(regiao_obj_1, regiao_obj_2):
+    if (regiao_obj_1[0] >= regiao_obj_2[0]):
+        valorInicial = regiao_obj_1[0]
+    else: valorInicial = regiao_obj_2[0]
+    if (regiao_obj_1[1] >= regiao_obj_2[1]):
+        valorFinal = regiao_obj_2[1]
+    else: valorFinal = regiao_obj_1[1]
     return [valorInicial, valorFinal]
 
-def areaOverlap(e1, e2):
-    coordX = linearOverlap(e1[0], e1[2], e2[0], e2[2])
-    coordY = linearOverlap(e1[1], e1[3], e2[1], e2[3])
+#devolve as coordenadas da região de sobreposição no formato x_inicial, y_inicial, x_final, y_final
+def hitBox(x1, x2, y1, y2):
+    coordX = linearOverlap(x1, x2)
+    coordY = linearOverlap(y1, y2)
     listafinal = [coordX[0], coordY[0], coordX[1], coordY[1]]
     return listafinal
 
+#função final que chama tudo
 def WordsOfRadiance(e1, e2):
-    if(interseptCheck(e1, e2)):
-        print("HIT: {} {} {} {}".join(areaOverlap(e1, e2)))
+    x1, y1 = eixo(e1)
+    x2, y2 = eixo(e2)
+    if(interseptCheck(x1, x2, y1, y2)):
+        print("HIT: {} {} {} {}".join(hitBox(x1, x2, y1, y2)))
     else:
         print("MISS")
 
-# Entradas das coordenadas
-entradaUm = input().split()
-entradaDois = input().split()
+# Entradas
+entradaUm = conversorEntradas(input().split())
+entradaDois = conversorEntradas(input().split())
+
+WordsOfRadiance(entradaUm, entradaDois)
