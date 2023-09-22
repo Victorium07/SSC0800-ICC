7c714e5285ee28502d7a2ffa7dfa3150291f9902
Victor <victor.h.alves7@gmail.com>
2023-09-22 16:41:39 -0300

    condições: astronauta já saiu e é a vez do astronauta sair, implementadas. Condição: astronauta está em espera, em desenvolvimento.

--- 03/Astronauta.py
+++ 03/Astronauta.py
@@ -2,27 +2,37 @@
 
 #funções úteis:
 
-
 def coleta():
     entrada = input().slice(' ')
     totalCelulas = entrada[0]**2
     movimentos = entrada[1]
     return (totalCelulas, movimentos)
 
-def caminhar(totEspacos):
-    pass
+def caminhar(espacos, passos):
+    msg = ''
+    if(espacos < passos):
+        pass
+    elif(espacos == passos):
+        pos = espacos/2 - 1
+        msg = f'O astronauta está na posição: {pos, pos} \n'
+    else: 
+        msg = ' \n'
+    return msg
 
 
 def checarMissao(espacos, passos):
     msg = ''
     if(espacos == passos):
-        msg = '\nPreste atencao, astronauta, chegou a sua vez!'
+        msg = 'Preste atencao, astronauta, chegou a sua vez!'
     elif(espacos < passos):
         msg = f'O astronauta ja saiu em missao ha {passos-espacos} chamadas.'
-    else: msg = f'\nAinda faltam {espacos-passos} chamadas para a sua vez!'
+    else: msg = f'Ainda faltam {espacos-passos} chamadas para a sua vez!'
     return msg
 
 def Oathbringer():
     totEspacos, movimentos = coleta()
     msg1 = x
     msg2 = checarMissao(totEspacos, movimentos)
+    print(msg1,msg2)
+
+Oathbringer()
